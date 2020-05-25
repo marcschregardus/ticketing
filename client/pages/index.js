ab63208a8c63f3be7cfa23f31eb5f4f4722a7e3e
Marc Schregardus <marc.schregardus@growthops.com.au>
2020-05-25 22:16:37 +1000

    Finished up sign up/in/out

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,16 @@
 import buildClient from '../api/build-client'
 
 const LandingPage = ({currentUser}) => {
-    console.log(currentUser);
-    // axios.get('/api/users/currentuser')
-    return <h1>Landing Page</h1>;
+    return currentUser ? (
+        <h1>You are signed in</h1>
+    ) : (
+        <h1>You are NOT signed in</h1>
+    );
 }
 
 // Server side calls (mostly!)
-LandingPage.getInitialProps = async (context) => {
+LandingPage.getInitialProps = async context => {
+    console.log('Landing Page!');
     let client = buildClient(context);
     const { data } = await client.get('/api/users/currentuser');
     return data;
