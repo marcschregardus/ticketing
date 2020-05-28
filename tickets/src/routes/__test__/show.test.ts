93bb5ee67ab620e68993af6c25c38e8e986cfdf5
Marc Schregardus <marc.schregardus@growthops.com.au>
2020-05-28 23:34:26 +1000

    Update the error handler to show the error message

---
+++ tickets/src/routes/__test__/show.test.ts
@@ -0,0 +1,34 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns a 404 if the ticket is not found', async () => {
+    const response = await request(app)
+        .get('/api/tickets/asdfasdfasdfasfsadfasfas')
+        .send();
+        // .expect(404);
+
+    console.log(response.body);
+});
+
+it('returns the ticket if it is found', async () => {
+
+    const title = 'Test Title';
+    const price = 20;
+
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title,
+            price,
+        })
+        .expect(201);
+
+    const ticketResponse = await request(app)
+        .get(`/api/tickets/${response.body.id}`)
+        .send()
+        .expect(200);
+
+    expect(ticketResponse.body.title).toEqual(title);
+    expect(ticketResponse.body.price).toEqual(price);
+});
