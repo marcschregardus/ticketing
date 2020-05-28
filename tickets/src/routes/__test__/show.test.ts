0e07ea0f632ac5759862bb64ca528e654f43f82f
Marc Schregardus <marc.schregardus@growthops.com.au>
2020-05-28 23:44:25 +1000

    fixed test

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,13 +1,13 @@
 import request from 'supertest';
 import { app } from '../../app';
+import mongoose from 'mongoose';
 
 it('returns a 404 if the ticket is not found', async () => {
-    const response = await request(app)
-        .get('/api/tickets/asdfasdfasdfasfsadfasfas')
-        .send();
-        // .expect(404);
-
-    console.log(response.body);
+    const id = new mongoose.Types.ObjectId().toHexString();
+    await request(app)
+        .get(`/api/tickets/${id}`)
+        .send()
+        .expect(404);
 });
 
 it('returns the ticket if it is found', async () => {
