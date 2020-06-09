9f4c162b4076f79757df3987cfb60c621fd0aa15
Marc Schregardus <marc.schregardus@growthops.com.au>
2020-06-09 20:37:45 +1000

    check for event version when updating

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -6,7 +6,7 @@ it('implements optimistic concurrency control', async (done) => {
     title: 'concert',
     price: 5,
     userId: 'ABC123',
-  })
+  });
 
   // Save the ticket to the database
   await ticket.save();
@@ -30,4 +30,19 @@ it('implements optimistic concurrency control', async (done) => {
   }
 
   throw new Error('Should not reach this point');
+});
+
+it('increments the version number on multiple saves', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 5,
+    userId: 'ABC123',
+  });
+
+  await ticket.save();
+  expect(ticket.version).toEqual(0);
+  await ticket.save();
+  expect(ticket.version).toEqual(1);
+  await ticket.save();
+  expect(ticket.version).toEqual(2);
 });
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -31,6 +31,7 @@ router.post('/api/tickets',
     await ticket.save();
     await new TicketCreatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
+      version: ticket.version,
       title: ticket.title,
       price: ticket.price,
       userId: ticket.userId,
