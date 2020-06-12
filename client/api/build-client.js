5e260510c58a21c7f5551591c4f1a0c0108b4c34
Marc Schregardus <marc.schregardus@growthops.com.au>
2020-06-12 11:02:18 +1000

    Create an list tickets

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default ({ req }) => {
-    if (typeof window === "undefined") {
+    if (typeof window === 'undefined') {
         // We are on the server
         return axios.create({
             baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
