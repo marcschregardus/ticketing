a62743cb19ccd6957fcbc968a339a4675c977cfa
Marc Schregardus <marc.schregardus@growthops.com.au>
2020-06-12 22:00:57 +1000

    Arbitrary change try and kick off build

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -3,6 +3,8 @@ import { natsWrapper } from './nats-wrapper';
 import { OrderCreatedListener } from './events/listeners/order-created-listener';
 
 const start = async () => {
+    console.log('Starting up the Expiration Service...');
+
     if (!process.env.NATS_CLUSTER_ID) {
         throw new Error('NATS_CLUSTER_ID must be defined');
     }
